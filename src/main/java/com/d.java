673165a05262892e69645b12f54815a74295ac1d
mxw <839506511@qq.com>
2022-11-15 18:50:23 +0800

    hot-fix commit1

--- src/main/java/com/d.java
+++ src/main/java/com/d.java
@@ -4,6 +4,6 @@ public class d {
     public  static void main(String[] args) {
         System.out.println("阿斯顿萨达");
         System.out.println("wwwsad的");
-        System.out.println("w33ad的");
+        System.out.println("w4ad的");
     }
 }
