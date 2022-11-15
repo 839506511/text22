03dc103395be154a0b5318ebfdea21676d670515
839506511 <110043730+839506511@users.noreply.github.com>
2022-11-15 20:00:06 +0800

    Update d.java

--- src/main/java/com/d.java
+++ src/main/java/com/d.java
@@ -2,11 +2,16 @@ package com;
 
 public class d {
     public  static void main(String[] args) {
-        System.out.println("阿斯顿萨达");
+            System.out.println("阿斯顿萨达");
         System.out.println("wwwsad的");
         System.out.println("w3ad的");
         System.out.println("w4ad的");
         System.out.println("master test");
         System.out.println("hot-fix test!");
+        System.out.println("gitee test!");
+         System.out.println("gitee test2!");
+          System.out.println("gitee test3!");
+            System.out.println("gitee test4!");
+
     }
 }
